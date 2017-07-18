5337c8bc8a11d679a37cd8c6cff0f8124608f175
bradonf333 <bradonf123@gmail.com>
2017-07-17 18:44:12 -0600

    created interface for a product

---
+++ APM/app/products/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    productld: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    price: number;
+    description: string;
+    starRating: number;
+    imageUr1: string;
+}
