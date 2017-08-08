a067401042a1b91df7af481aa5b51eda81e9117f
bradonf333 <bradonf123@gmail.com>
2017-08-07 19:34:27 -0600

    Removed unneeded import statements

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -3,13 +3,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ProductListComponent } from './products/product-list.component';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductFilterPipe } from './products/product-filter.pipe';
-import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductDetailGuard } from './products/product-guard.service';
 
