f65588392f9ee7572d4e28c550333a3b4910e480
bradonf333 <bradonf123@gmail.com>
2017-08-07 19:44:13 -0600

    Removed unneeded imports

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -2,12 +2,10 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ProductListComponent } from './products/product-list.component';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductDetailGuard } from './products/product-guard.service';
 
 import { WelcomeComponent } from './home/welcome.component';
